47a4e88d0573eedf95a8ed400993fbe22d201ff2
JSLifeform <thecheapgeekz@gmail.com>
2021-11-14 16:05:04 -0500

    functional but all in main package, attempting to separate into custom packages

--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/xuri/excelize/v2"
+	// "Bourbon_Database/pkg/createDB"
 )
 
-type Tag struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+const (
+	IntegerCol = "INT"
+	NumericCol = "NUMERIC(6,2)"
+	StringCol  = "VARCHAR(255)"
+	FilePath   = `Bourbon_Website_Data.xlsx`
+	Database   = `testdb`
+)
+
+type queryResult struct {
+	ID            int     `json:"id"`
+	Bourbon       string  `json:"bourbon"`
+	Distillery    string  `json:"distillery"`
+	City          string  `json:"city"`
+	State         string  `json:"state"`
+	Age           int     `json:"age"`
+	Proof         float32 `json:"proof"`
+	Estimated_Age string  `json:"estimated_age"`
+	Aroma_Notes   string  `json:"aroma_notes"`
+	Flavor_Notes  string  `json:"flavor_notes"`
+	Finish_Notes  string  `json:"finish_notes"`
+	Mashbill      string  `json:"mashbill"`
+	Sourced       string  `json:"sourced"`
+	Sourced_From  string  `json:"sourced_from"`
+	Released      string  `json:"released"`
 }
 
 func main() {
@@ -43,22 +67,155 @@ func main() {
 	// defer insert.Close()
 
 	// test select statement
-	results, err := db.Query("SELECT id, testcol FROM test")
+
+	// open Excel file
+	f, err := excelize.OpenFile(FilePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// rows, _ := openSpreadsheet(filePath)
+
+	// read in rows individually
+	rows, err := f.GetRows("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//drops old table
+	dropTable := "DROP TABLE IF EXISTS " + Database + ";"
+	DropResults, err := db.Query(dropTable)
+	if err != nil {
+		fmt.Println("Panic during table drop!")
+		panic(err.Error())
+	}
+	fmt.Print("New table created! Results of db.Query: ", &DropResults, "\n\n\n")
+
+	// loops through rows in sheet
+	for i, row := range rows {
+
+		// skips first row of senseless data
+		if i == 0 {
+			continue
+
+			// uses second row to create database with names of columns
+		} else if i == 1 {
+
+			var inputs []string
+			for _, colCell := range row {
+				// string for user to input column type
+				var input string
+				// ask for and reads in data type
+				fmt.Println("What data type(int, str, num) do you want for column ", colCell, "?")
+				fmt.Scanln(&input)
+				if input == "str" {
+					addToInput := colCell + " " + StringCol + ", "
+					inputs = append(inputs, addToInput)
+				} else if input == "int" {
+					addToInput := colCell + " " + IntegerCol + ", "
+					inputs = append(inputs, addToInput)
+				} else if input == "num" {
+					addToInput := colCell + " " + NumericCol + ", "
+					inputs = append(inputs, addToInput)
+				} else {
+
+					// quits out of program, would like to prompt user again for correct input
+					fmt.Println("Incorrect input given, quitting until I figure out how to restart the loop and get correct input")
+					return
+				}
+
+			}
+			tableCreate := "CREATE TABLE IF NOT EXISTS " + Database + "(\n" + "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+			for _, item := range inputs {
+				tableCreate += item
+			}
+			// trims trailing ", " from tableCreate string
+			tableCreate = strings.Trim(tableCreate, ", ")
+
+			// appends trailing ");" to SQL query
+			tableCreate += ");"
+
+			// attempts to create table with SQL query
+			fmt.Print(tableCreate, "\n\n\n")
+			insert, err := db.Query(tableCreate)
+			if err != nil {
+				fmt.Println("Error on table create!")
+				panic(err.Error())
+			}
+
+			// close insert
+			defer insert.Close()
+
+		} else {
+			// this block does individual insert statements
+
+			// string variable to add INSERT statement to
+			addTable := "INSERT INTO " + Database + " VALUES ( NULL, "
+
+			// loops through cells in row
+			for _, colCell := range row {
+				fmt.Println(colCell)
+
+				// if colCell is empty add NULL statement to addTable query, otherwise add colCell information
+				if colCell == "" {
+					addTable += "NULL, "
+				} else {
+					addTable += `"` + colCell + `", `
+				}
+
+			}
+
+			// trims trailing ', ' from string before adding closing line to statement
+			addTable = strings.Trim(addTable, ", ")
+			// adds trailing parentheses and semicolon to INSERT statement
+			addTable += ");\n"
+
+			// prints result and attempts insert query
+			fmt.Println("Result is: ", addTable)
+			insert, err := db.Query(addTable)
+			if err != nil {
+				fmt.Print("Error on ", addTable, "\n\n")
+				fmt.Println(err)
+				panic(err.Error())
+			}
+
+			// close insert statement
+			defer insert.Close()
+		}
+	}
+
+	results, err := db.Query("SELECT * FROM " + Database)
 	if err != nil {
 		fmt.Println("Panic on select statement")
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
 
+	//close query
+	defer results.Close()
+
 	for results.Next() {
-		var tag Tag
-		// for each row, scan the result into our tag composite object
-		err = results.Scan(&tag.ID, &tag.Name)
+
+		var PrintQuery queryResult
+		err = results.Scan(&PrintQuery.ID, &PrintQuery.Bourbon, &PrintQuery.Distillery, &PrintQuery.City, &PrintQuery.State, &PrintQuery.Age, &PrintQuery.Proof,
+			&PrintQuery.Estimated_Age, &PrintQuery.Aroma_Notes, &PrintQuery.Flavor_Notes, &PrintQuery.Finish_Notes, &PrintQuery.Mashbill, &PrintQuery.Sourced,
+			&PrintQuery.Sourced_From, &PrintQuery.Released)
 		if err != nil {
 			fmt.Println("Panic on Scan")
 			panic(err.Error()) // proper error handling instead of panic in your app
 		}
-		// and then print out the tag's Name attribute
-		fmt.Println(tag.Name)
+		fmt.Println(PrintQuery)
+
+		// // var tag Tag
+		// // for each row, scan the result into our tag composite object
+		// err := results.Scan(&printStr)
+		// if err != nil {
+		// 	fmt.Println("Panic on Scan")
+		// 	panic(err.Error()) // proper error handling instead of panic in your app
+		// }
+		// // and then print out the tag's Name attribute
+		// fmt.Println(printStr)
 	}
 
 }
--- pkg/createDB.go
+++ pkg/createDB.go
@@ -1,134 +1,162 @@
-package main
+package createDB
 
-import (
-	"fmt"
-	"strings"
-
-	"github.com/xuri/excelize/v2"
-)
-
-const (
-	integerCol = "INT"
-	numericCol = "NUMERIC(6,2)"
-	stringCol  = "VARCHAR(45)"
-	filePath   = `Bourbon_Website_Data.xlsx`
-	database   = `testdb`
-)
-
-func main() {
-	//columnlist := []string("")
-
-	// open Excel file
-	f, err := excelize.OpenFile(filePath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// read in rows individually
-	rows, err := f.GetRows("Sheet1")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// loops through rows in sheet
-	for i, row := range rows {
-
-		// skips first row of senseless data
-		if i == 0 {
-			continue
-
-			// uses second row to create database with names of columns
-		} else if i == 1 {
-
-			var inputs []string
-			for _, colCell := range row {
-				// string for user to input column type
-				var input string
-				// ask for and reads in data type
-				fmt.Println("What data type(int, str, num) do you want for column ", colCell, "?")
-				fmt.Scanln(&input)
-				if input == "str" {
-					addToInput := colCell + " " + stringCol + ", "
-					inputs = append(inputs, addToInput)
-				} else if input == "int" {
-					addToInput := colCell + " " + integerCol + ", "
-					inputs = append(inputs, addToInput)
-				} else if input == "num" {
-					addToInput := colCell + " " + numericCol + ", "
-					inputs = append(inputs, addToInput)
-				} else {
-
-					// quits out of program, would like to prompt user again for correct input
-					fmt.Println("Incorrect input given, quitting until I figure out how to restart the loop and get correct input")
-					return
-				}
-
-			}
-			tableCreate := "CREATE TABLE [IF NOT EXISTS] " + database + "(\n" + "id INT AUTO_INCREMENT PRIMARY KEY, "
-			for _, item := range inputs {
-				tableCreate += item
-			}
-			// trims trailing ", " from tableCreate string
-			tableCreate = strings.Trim(tableCreate, ", ")
-
-			// appends trailing ");" to SQL query
-			tableCreate += ");"
-			fmt.Print(tableCreate, "\n\n\n")
-		} else {
-
-			// string variable to add INSERT statement to
-			addTable := "INSERT INTO " + database + " VALUES ("
-
-			// loops through cells in row
-			for _, colCell := range row {
-				addTable += `"` + colCell + `", `
-			}
-
-			// trims trailing ', ' from string before adding closing line to statement
-			addTable = strings.Trim(addTable, ", ")
-			// adds trailing parentheses and semicolon to INSERT statement
-			addTable += ");\n"
-			// I think adds new line, delete?
-			fmt.Println(addTable)
-		}
-	}
-	// shows opened file
-	fmt.Println(filePath)
-
+func dropTable(dbName string) string {
+	dropSyntax := "DROP TABLE IF EXISTS " + dbName + ";"
+	return dropSyntax
 }
 
-/*
-package main
+// import (
+// 	"fmt"
+// 	"strings"
 
-import (
-	"fmt"
-)
+// 	"github.com/xuri/excelize/v2"
+// )
 
-func getColType() string {
-	for {
-		var input string
-		// ask for and reads in data type
-		fmt.Println("What data type(int, str, num) do you want for column ", colCell, "?")
-		fmt.Scanln(&input)
-		switch input {
-		case "str":
-			return StrColType
-		case "int":
-			return IntColType
-		case "num":
-			return NumColType
-		default:
-			fmt.Println("Incorrect input given, quitting until I figure out how to restart the loop and get correct input")
-		}
-	}
-}
+// const (
+// 	integerCol = "INT"
+// 	numericCol = "NUMERIC(6,2)"
+// 	stringCol  = "VARCHAR(45)"
+// 	filePath   = `Bourbon_Website_Data.xlsx`
+// 	database   = `testdb`
+// )
 
-func main() {
-	// string for user to input column type
+// // func openSpreadsheet(filePath string) ([]string, error) {
+// // 		// open Excel file
+// // 		f, err := excelize.OpenFile(filePath)
+// // 		if err != nil {
+// // 			fmt.Println("Error while opening spreadsheet: ", err)
+// // 			return nil, err
+// // 		}
+// // 		fmt.Println(f)
+// // 		fmt.Println(&f)
+// // 		fmt.Println(*f)
 
-	addToInput := colCell + " " + colType + ", "
-	inputs = append(inputs, addToInput)
-}
-*/
+// // 		rows, err := f.GetRows("Sheet1")
+// // 		if err != nil {
+// // 			fmt.Println(err)
+// // 			return nil, err
+// // 		}
+// // 		return rows, nil
+// // }
+
+// func main() {
+// 	//columnlist := []string("")
+
+// 	// open Excel file
+// 	f, err := excelize.OpenFile(filePath)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	// rows, _ := openSpreadsheet(filePath)
+
+// 	// read in rows individually
+// 	rows, err := f.GetRows("Sheet1")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Println(rows)
+// 	fmt.Println(&rows)
+
+// 	// loops through rows in sheet
+// 	for i, row := range rows {
+
+// 		// skips first row of senseless data
+// 		if i == 0 {
+// 			continue
+
+// 			// uses second row to create database with names of columns
+// 		} else if i == 1 {
+
+// 			var inputs []string
+// 			for _, colCell := range row {
+// 				// string for user to input column type
+// 				var input string
+// 				// ask for and reads in data type
+// 				fmt.Println("What data type(int, str, num) do you want for column ", colCell, "?")
+// 				fmt.Scanln(&input)
+// 				if input == "str" {
+// 					addToInput := colCell + " " + stringCol + ", "
+// 					inputs = append(inputs, addToInput)
+// 				} else if input == "int" {
+// 					addToInput := colCell + " " + integerCol + ", "
+// 					inputs = append(inputs, addToInput)
+// 				} else if input == "num" {
+// 					addToInput := colCell + " " + numericCol + ", "
+// 					inputs = append(inputs, addToInput)
+// 				} else {
+
+// 					// quits out of program, would like to prompt user again for correct input
+// 					fmt.Println("Incorrect input given, quitting until I figure out how to restart the loop and get correct input")
+// 					return
+// 				}
+
+// 			}
+// 			tableCreate := "CREATE TABLE [IF NOT EXISTS] " + database + "(\n" + "id INT AUTO_INCREMENT PRIMARY KEY, "
+// 			for _, item := range inputs {
+// 				tableCreate += item
+// 			}
+// 			// trims trailing ", " from tableCreate string
+// 			tableCreate = strings.Trim(tableCreate, ", ")
+
+// 			// appends trailing ");" to SQL query
+// 			tableCreate += ");"
+// 			fmt.Print(tableCreate, "\n\n\n")
+// 		} else {
+
+// 			// string variable to add INSERT statement to
+// 			addTable := "INSERT INTO " + database + " VALUES ("
+
+// 			// loops through cells in row
+// 			for _, colCell := range row {
+// 				addTable += `"` + colCell + `", `
+// 			}
+
+// 			// trims trailing ', ' from string before adding closing line to statement
+// 			addTable = strings.Trim(addTable, ", ")
+// 			// adds trailing parentheses and semicolon to INSERT statement
+// 			addTable += ");\n"
+// 			// I think adds new line, delete?
+// 			fmt.Println(addTable)
+// 		}
+// 	}
+// 	// shows opened file
+// 	fmt.Println(filePath)
+
+// }
+
+// /*
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func getColType() string {
+// 	for {
+// 		var input string
+// 		// ask for and reads in data type
+// 		fmt.Println("What data type(int, str, num) do you want for column ", colCell, "?")
+// 		fmt.Scanln(&input)
+// 		switch input {
+// 		case "str":
+// 			return StrColType
+// 		case "int":
+// 			return IntColType
+// 		case "num":
+// 			return NumColType
+// 		default:
+// 			fmt.Println("Incorrect input given, quitting until I figure out how to restart the loop and get correct input")
+// 		}
+// 	}
+// }
+
+// // func main() {
+// 	// string for user to input column type
+
+// 	addToInput := colCell + " " + colType + ", "
+// 	inputs = append(inputs, addToInput)
+// }
+// */
